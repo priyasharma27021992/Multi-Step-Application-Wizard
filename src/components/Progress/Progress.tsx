bffe47896a008cc911c9be3fb24824ee73f4ba0d
Priya Sharma <priyasharma2702@gmail.com>
2025-12-24 21:35:47 +0530

    adding new progress as this one is slow and not that great from performncae pov

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,50 +1,5 @@
 const Progress = ({ currentStep = 1 }) => {
-	//5 steps
-	//5 circles
-	//4 lines
-	//5 circles showing step
-
-	return (
-		<div
-			className='flex'
-			style={{ flexDirection: 'row', gap: '16px', position: 'relative' }}>
-			<div
-				style={{
-					width: '10px',
-					height: '10px',
-					backgroundColor: 'red',
-					borderRadius: '100%',
-				}}></div>
-			<div
-				style={{
-					width: '10px',
-					height: '10px',
-					backgroundColor: 'red',
-					borderRadius: '100%',
-				}}></div>
-			<div
-				style={{
-					width: '10px',
-					height: '10px',
-					backgroundColor: 'red',
-					borderRadius: '100%',
-				}}></div>
-			<div
-				style={{
-					width: '10px',
-					height: '10px',
-					backgroundColor: 'red',
-					borderRadius: '100%',
-				}}></div>
-			<div
-				style={{
-					width: '10px',
-					height: '10px',
-					backgroundColor: 'red',
-					borderRadius: '100%',
-				}}></div>
-		</div>
-	);
+	return <div>Changing Progress</div>;
 };
 
 export default Progress;

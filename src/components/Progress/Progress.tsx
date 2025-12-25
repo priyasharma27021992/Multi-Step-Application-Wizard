ba1eaa9e5ce0207aeba6f380529caad4eec5492a
Priya Sharma <priyasharma2702@gmail.com>
2025-12-26 00:07:40 +0530

    added new progress

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,5 +1,27 @@
-const Progress = ({ currentStep = 1 }) => {
-	return <div>Changing Progress</div>;
+import styles from './Progress.module.css';
+
+interface ProgressProps {
+	currentStep: number;
+	steps?: number;
+}
+
+const Progress = ({ currentStep = 0, steps = 5 }: ProgressProps) => {
+	return (
+		<div className={styles.progress}>
+			<div className={styles.progress__line}></div>
+			{Array(steps)
+				.fill(0)
+				.map((step, index) => {
+					return (
+						<div
+							key={step}
+							className={`${styles.progress__step} ${
+								index <= currentStep ? styles.progress__step__active : ''
+							}`}></div>
+					);
+				})}
+		</div>
+	);
 };
 
 export default Progress;
